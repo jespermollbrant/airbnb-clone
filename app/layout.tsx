98d1534de8613d4d45b58080a97dc473c5da7f8e
jespermollbrant <jesperviktormollbrant@gmail.com>
2024-12-07 12:52:26 +0100

    Update application metadata: change title from "Airbnb clone" to "Blocki" and update description to reflect a seamless exchange.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ import SearchModal from "./components/modals/SearchModal";
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Airbnb clone",
-  description: "Airbnb app for vacation rentals",
+  title: "Blocki",
+  description: "Blocki for a seamless exchange",
 };
 
 export default async function RootLayout({
